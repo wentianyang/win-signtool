b9f4d7f2c6969054aa4142b68e5fdaa515e4d808
yangtianwen <yangtianwen@hudunsoft.com>
2023-04-06 18:25:22 +0800

    fix: 上传文件失败

--- server/server.go
+++ server/server.go
@@ -5,10 +5,6 @@ import (
 	"io"
 	"net/http"
 	"os"
-	"path/filepath"
-
-	"github.com/alexflint/go-filemutex"
-	"github.com/yangtianwen/win-signtool/sign"
 )
 
 // 使用 gin 框架,创建一个文件上传服务
@@ -29,7 +25,7 @@ func Server() {
 
 func uploadFile(w http.ResponseWriter, r *http.Request) {
 	// 解析表单
-	// r.ParseMultipartForm(10 << 20) // 10MB
+	r.ParseMultipartForm(10 << 20) // 10MB
 
 	// 获取文件句柄
 	file, handler, err := r.FormFile("file")
@@ -45,40 +41,15 @@ func uploadFile(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("File Size: %+v\n", handler.Size)
 	fmt.Printf("MIME Type: %+v\n", handler.Header.Get("Content-Type"))
 
-	// 创建文件
+	// 写入文件到服务器
 	f, err := os.OpenFile(handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
 		fmt.Println("Error Saving File")
 		fmt.Println(err)
 		return
 	}
-
-	// 获取文件锁
-	mutex, _ := filemutex.New(filepath.Join(".", handler.Filename))
-	if err := mutex.Lock(); err != nil {
-		fmt.Println("Error Acquiring File Lock")
-		fmt.Println(err)
-		f.Close()
-		return
-	}
-	defer mutex.Unlock()
-
-	// 写入文件到服务器
-	_, err = io.Copy(f, file)
-	if err != nil {
-		fmt.Println("Error Writing File Content")
-		fmt.Println(err)
-		f.Close()
-		return
-	}
-
-	// 关闭文件
-	f.Close()
+	defer f.Close()
+	io.Copy(f, file)
 
 	fmt.Fprintf(w, "Successfully Uploaded File\n")
-
-	// 执行签名
-	sign.Sign(handler.Filename, func(filename string) {
-		fmt.Fprintf(w, "Successfully Signed File: %s\n", filename)
-	})
 }
